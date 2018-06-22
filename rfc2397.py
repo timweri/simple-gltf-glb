37d5d23b810e0169e08103d99d124e547e917ddf
TimWeri <ng.duc.tahn@gmail.com>
2018-06-23 01:30:20 +0700

    Functional encoder
    Not fully tested

--- rfc2397.py
+++ rfc2397.py
@@ -16,51 +16,62 @@ import struct
 
 
 class RFC2397:
-    COMPONENTTYPE_MAP: {
-        5120: 'b',      # char
-        5121: 'B',      # unsigned char
-        5122: '<h',     # short
-        5123: '<H',     # unsigned short
-        5125: '<I',     # unsigned int
-        5126: '<f'
-    }
-
     def __init__(self):
         pass
 
     @staticmethod
-    def converter_5126(inp):
-        """Convert a float into a base32 4-byte bytes object and return the bytes object"""
-        out = struct.pack('<f', inp)
+    def _get_comptype_formatter(comptype_id):
+        COMPONENTTYPE_MAP = {
+            5120: 'b',  # char
+            5121: 'B',  # unsigned char
+            5122: '<h',  # short
+            5123: '<H',  # unsigned short
+            5125: '<I',  # unsigned int
+            5126: '<f'
+        }
+        return COMPONENTTYPE_MAP[comptype_id]
+
+    @staticmethod
+    def _get_compnumber(ele_type):
+        TYPE_MAP = {
+            "SCALAR": 1,
+            "VEC2": 2,
+            "VEC3": 3,
+            "VEV4": 4,
+            "MAT2": 4,
+            "MAT3": 9,
+            "MAT4": 16
+        }
+        return TYPE_MAP[ele_type]
+
+    @staticmethod
+    def _get_comp_bobj(inp, comptype_id):
+        """Convert an input into a base32 byte object according to the component type and return the bytes object"""
+        out = struct.pack(RFC2397._get_comptype_formatter(comptype_id), inp)
 
         return out
 
     @staticmethod
-    def _encode_list(lst, ele_converter):
+    def encode_list(lst, comptype_id, ele_type):
         """Encode a list to a glTF compliant base64-encoded string
 
         :param lst: the list of elements to be converted to a glTF compliant bytearray.
-        :param ele_converter: A function that returns a glTF compliant bytes object representation of list elements.
-                              Each return will have the same byte size.
+        :param comptype_id: the id that defines the type of each component.
+        :param ele_type: a string that specifies the type of each element.
         :return: glTF compliant base64-encoded string that represents the input list
         """
 
         final_barray = 0
+        comp_number = RFC2397._get_compnumber(ele_type)
 
         for ele in lst:
-            bobj = ele_converter(ele)
-            if not final_barray:
-                final_barray = bytearray(bobj)
-            else:
-                final_barray.extend(bobj)
+            for i in range(comp_number):
+                bobj = RFC2397._get_comp_bobj(ele if comp_number == 1 else ele[i], comptype_id)
+                if not final_barray:
+                    final_barray = bytearray(bobj)
+                else:
+                    final_barray.extend(bobj)
 
         output = base64.b64encode(final_barray)
 
         return output
-
-
-lst = [1,0.3,0.2,0,0.9,-0.2]
-
-print(RFC2397.converter_5126(0.2))
-
-print(RFC2397._encode_list(lst,RFC2397.converter_5126))
