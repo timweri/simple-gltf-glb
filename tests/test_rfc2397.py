db679b97059a4e3a2d4c7306aaef3767ec54f5d9
Tim Weri <ng.duc.tahn@gmail.com>
2018-07-01 16:26:40 +0700

    Give each small test one method and rewrite tests to work with new structure

--- tests/test_rfc2397.py
+++ tests/test_rfc2397.py
@@ -5,45 +5,83 @@ from rfc2397 import RFC2397
 class TestRFC2397(unittest.TestCase):
     """Tests for `rfc2397.py`."""
 
-    def test_get_embedded_uri(self):
+    def test_get_embedded_uri1(self):
         # Taken from https://github.com/KhronosGroup/glTF-Sample-Models/tree/master/2.0/TriangleWithoutIndices
-        test_data = {
+        test_data = [{
             "data": [[0, 0, 0], [1, 0, 0], [0, 1, 0]],
             "comptype_id": 5126,
-            "ele_type": "VEC3"
-        }
+            "ele_type": "VEC3",
+            "vertex_attr": True
+        }]
 
-        self.assertEqual(RFC2397.get_embedded_uri(**test_data),
-                         ("data:application/octet-stream;base64,AAAAAAAAAAAAAAAAAACAPwAAAAAAAAAAAAAAAAAAgD8AAAAA",
-                          36))
+        uri, length, buffer_view_data = RFC2397.get_embedded_uri(test_data)
+        self.assertEqual(uri, "data:application/octet-stream;base64,AAAAAAAAAAAAAAAAAACAPwAAAAAAAAAAAAAAAAAAgD8AAAAA")
+        self.assertEqual(length, 36)
+        self.assertEqual(buffer_view_data,
+                         [
+                             {
+                                 "byteLength": 36,
+                                 "byteOffset": 0,
+                                 "byteStride": 12
+                             }
+                         ])
 
-        test_data = {
+    def test_get_embedded_uri2(self):
+        test_data = [{
             "data": [1, 2, 3, 4],
             "comptype_id": 5120,
-            "ele_type": "SCALAR"
-        }
+            "ele_type": "SCALAR",
+            "vertex_attr": False
+        }]
 
-        self.assertEqual(RFC2397.get_embedded_uri(**test_data),
-                         ("data:application/octet-stream;base64,AQIDBA==",
-                          4))
+        uri, length, buffer_view_data = RFC2397.get_embedded_uri(test_data)
+        self.assertEqual(uri, "data:application/octet-stream;base64,AQIDBA==")
+        self.assertEqual(length, 4)
+        self.assertEqual(buffer_view_data,
+                         [
+                             {
+                                 "byteLength": 4,
+                                 "byteOffset": 0,
+                                 "byteStride": 1
+                             }
+                         ])
 
-        test_data = {
-            "data": [[[1, 2], [3, 4]]],
+    def test_get_embedded_uri3(self):
+        test_data = [{
+            "data": [[1, 2, 3, 4]],
             "comptype_id": 5122,
-            "ele_type": "MAT2"
-        }
+            "ele_type": "MAT2",
+            "vertex_attr": False
+        }]
 
-        self.assertEqual(RFC2397.get_embedded_uri(**test_data),
-                         ("data:application/octet-stream;base64,AQACAAMABAA=",
-                          8))
+        uri, length, buffer_view_data = RFC2397.get_embedded_uri(test_data)
+        self.assertEqual(uri, "data:application/octet-stream;base64,AQACAAMABAA=")
+        self.assertEqual(length, 8)
+        self.assertEqual(buffer_view_data,
+                         [
+                             {
+                                 "byteLength": 8,
+                                 "byteOffset": 0,
+                                 "byteStride": 8
+                             }
+                         ])
 
-        test_data = {
-            "data": [[[1, 2, 3], [2, 3, 4], [3, 4, 5]],
-                     [[5, 5, 5], [6, 6, 6], [7, 7, 7]]],
+    def test_get_embedded_uri4(self):
+        test_data = [{
+            "data": [[1, 2, 3, 2, 3, 4, 3, 4, 5], [5, 5, 5, 6, 6, 6, 7, 7, 7]],
             "comptype_id": 5122,
-            "ele_type": "MAT3"
-        }
+            "ele_type": "MAT3",
+            "vertex_attr": False
+        }]
 
-        self.assertEqual(RFC2397.get_embedded_uri(**test_data),
-                         ("data:application/octet-stream;base64,AQACAAMAAgADAAQAAwAEAAUABQAFAAUABgAGAAYABwAHAAcA",
-                          36))
+        uri, length, buffer_view_data = RFC2397.get_embedded_uri(test_data)
+        self.assertEqual(uri, "data:application/octet-stream;base64,AQACAAMAAgADAAQAAwAEAAUABQAFAAUABgAGAAYABwAHAAcA")
+        self.assertEqual(length, 36)
+        self.assertEqual(buffer_view_data,
+                         [
+                             {
+                                 "byteLength": 36,
+                                 "byteOffset": 0,
+                                 "byteStride": 18
+                             }
+                         ])
