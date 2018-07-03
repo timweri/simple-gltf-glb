0af4c3351831425d9c48f0d0fda58baae7093d57
Tim Weri <ng.duc.tahn@gmail.com>
2018-07-04 00:04:15 +0700

    Test embedding multiple data types into one buffer

--- tests/test_gltf.py
+++ tests/test_gltf.py
@@ -1,5 +1,6 @@
 import unittest
 from gltf import GLTF
+import filecmp
 
 
 # TODO: Finish test cases for all public methods
@@ -50,15 +51,19 @@ class TestConverter(unittest.TestCase):
         """This test data is taken from https://github.com/KhronosGroup/glTF-Sample-Models/blob/master/2.0/Triangle/
         """
         gltf_asset = GLTF()
-        gltf_asset.create_accessor_set(name="vertices",
-                                       data=[[0, 0, 0], [1, 0, 0], [0, 1, 0]],
-                                       ele_type="VEC3",
-                                       comptype_id=5126,
-                                       count=3,
-                                       max_vals=[1.0, 1.0, 0.0],
-                                       min_vals=[0.0, 0.0, 0.0],
-                                       byte_offset=0,
-                                       target=34962)
+        gltf_asset.embed_data(buffer_name="vertices",
+                              accessor_data=[{
+                                  "name": "vertices",
+                                  "data": [[0, 0, 0], [1, 0, 0], [0, 1, 0]],
+                                  "ele_type": "VEC3",
+                                  "comptype_id": 5126,
+                                  "count": 3,
+                                  "max_vals": [1.0, 1.0, 0.0],
+                                  "min_vals": [0.0, 0.0, 0.0],
+                                  "byte_offset": 0,
+                                  "target": 34962,
+                                  "vertex_attr": True
+                              }])
         gltf_asset.create_primitive(name="primitive1",
                                     attributes={
                                         "POSITION": "vertices"
@@ -120,28 +125,36 @@ class TestConverter(unittest.TestCase):
                              })
 
     def test_full_asset2(self):
-        """This test data is adapted from https://github.com/KhronosGroup/glTF-Sample-Models/tree/master/2.0/Triangle
-        The original test data is not yet recreatable since, for now, buffer can only hold one data type.
+        """This test data is taken from https://github.com/KhronosGroup/glTF-Sample-Models/tree/master/2.0/Triangle
         """
         gltf_asset = GLTF()
-        gltf_asset.create_accessor_set(name="indices",
-                                       ele_type="SCALAR",
-                                       comptype_id=5123,
-                                       target=34963,
-                                       count=3,
-                                       max_vals=[2],
-                                       min_vals=[0],
-                                       byte_offset=0,
-                                       data=[0, 1, 2])
-        gltf_asset.create_accessor_set(name="vertices",
-                                       ele_type="VEC3",
-                                       comptype_id=5126,
-                                       target=34962,
-                                       count=3,
-                                       max_vals=[1.0, 1.0, 0.0],
-                                       min_vals=[0.0, 0.0, 0.0],
-                                       byte_offset=0,
-                                       data=[[0, 0, 0], [1, 0, 0], [0, 1, 0]])
+        gltf_asset.embed_data(buffer_name="buffer1",
+                              accessor_data=[
+                                  {
+                                      "name": "indices",
+                                      "ele_type": "SCALAR",
+                                      "comptype_id": 5123,
+                                      "target": 34963,
+                                      "count": 3,
+                                      "max_vals": [2],
+                                      "min_vals": [0],
+                                      "byte_offset": 0,
+                                      "data": [0, 1, 2],
+                                      "vertex_attr": False
+                                  },
+                                  {
+                                      "name": "vertices",
+                                      "ele_type": "VEC3",
+                                      "comptype_id": 5126,
+                                      "target": 34962,
+                                      "count": 3,
+                                      "max_vals": [1.0, 1.0, 0.0],
+                                      "min_vals": [0.0, 0.0, 0.0],
+                                      "byte_offset": 0,
+                                      "data": [[0, 0, 0], [1, 0, 0], [0, 1, 0]],
+                                      "vertex_attr": True
+                                  }
+                              ])
         gltf_asset.create_primitive(name="primitive1",
                                     attributes={
                                         "POSITION": "vertices"
@@ -175,13 +188,9 @@ class TestConverter(unittest.TestCase):
                                  ],
                                  "buffers": [
                                      {
-                                         "uri": "data:application/octet-stream;base64,AAABAAIA",
-                                         "byteLength": 6
-                                     },
-                                     {
-                                         "uri": "data:application/octet-stream;base64,AAAAAAAAAAAAAAAAAACAPwAAAAAAAAAAA"
-                                                "AAAAAAAgD8AAAAA",
-                                         "byteLength": 36
+                                         "uri": "data:application/octet-stream;base64,AAABAAIAAAAAAAAAAAAAAAAAAAAAAIA/A"
+                                                "AAAAAAAAAAAAAAAAACAPwAAAAA=",
+                                         "byteLength": 44
                                      }
                                  ],
                                  "bufferViews": [
@@ -192,8 +201,8 @@ class TestConverter(unittest.TestCase):
                                          "target": 34963
                                      },
                                      {
-                                         "buffer": 1,
-                                         "byteOffset": 0,
+                                         "buffer": 0,
+                                         "byteOffset": 8,
                                          "byteLength": 36,
                                          "target": 34962
                                      }
@@ -223,3 +232,114 @@ class TestConverter(unittest.TestCase):
                                      "version": "2.0"
                                  }
                              })
+
+    def test_export_gltf1(self):
+        """This test data is taken from https://github.com/KhronosGroup/glTF-Sample-Models/tree/master/2.0/Triangle
+        """
+        gltf_asset = GLTF()
+        gltf_asset.embed_data(buffer_name="buffer1",
+                              accessor_data=[
+                                  {
+                                      "name": "indices",
+                                      "ele_type": "SCALAR",
+                                      "comptype_id": 5123,
+                                      "target": 34963,
+                                      "count": 3,
+                                      "max_vals": [2],
+                                      "min_vals": [0],
+                                      "byte_offset": 0,
+                                      "data": [0, 1, 2],
+                                      "vertex_attr": False
+                                  },
+                                  {
+                                      "name": "vertices",
+                                      "ele_type": "VEC3",
+                                      "comptype_id": 5126,
+                                      "target": 34962,
+                                      "count": 3,
+                                      "max_vals": [1.0, 1.0, 0.0],
+                                      "min_vals": [0.0, 0.0, 0.0],
+                                      "byte_offset": 0,
+                                      "data": [[0, 0, 0], [1, 0, 0], [0, 1, 0]],
+                                      "vertex_attr": True
+                                  }
+                              ])
+        gltf_asset.create_primitive(name="primitive1",
+                                    attributes={
+                                        "POSITION": "vertices"
+                                    },
+                                    indices="indices")
+        gltf_asset.create_mesh(name="mesh1", primitives=["primitive1"])
+        gltf_asset.create_node(name="node1", mesh="mesh1")
+        gltf_asset.create_scene(name="scene1", nodes=["node1"])
+
+        self.assertDictEqual(gltf_asset.to_dict(),
+                             {
+                                 "scenes": [
+                                     {
+                                         "nodes": [0]
+                                     }
+                                 ],
+                                 "nodes": [
+                                     {
+                                         "mesh": 0
+                                     }
+                                 ],
+                                 "meshes": [
+                                     {
+                                         "primitives": [{
+                                             "attributes": {
+                                                 "POSITION": 1
+                                             },
+                                             "indices": 0
+                                         }]
+                                     }
+                                 ],
+                                 "buffers": [
+                                     {
+                                         "uri": "data:application/octet-stream;base64,AAABAAIAAAAAAAAAAAAAAAAAAAAAAIA/A"
+                                                "AAAAAAAAAAAAAAAAACAPwAAAAA=",
+                                         "byteLength": 44
+                                     }
+                                 ],
+                                 "bufferViews": [
+                                     {
+                                         "buffer": 0,
+                                         "byteOffset": 0,
+                                         "byteLength": 6,
+                                         "target": 34963
+                                     },
+                                     {
+                                         "buffer": 0,
+                                         "byteOffset": 8,
+                                         "byteLength": 36,
+                                         "target": 34962
+                                     }
+                                 ],
+                                 "accessors": [
+                                     {
+                                         "bufferView": 0,
+                                         "byteOffset": 0,
+                                         "componentType": 5123,
+                                         "count": 3,
+                                         "type": "SCALAR",
+                                         "max": [2],
+                                         "min": [0]
+                                     },
+                                     {
+                                         "bufferView": 1,
+                                         "byteOffset": 0,
+                                         "componentType": 5126,
+                                         "count": 3,
+                                         "type": "VEC3",
+                                         "max": [1.0, 1.0, 0.0],
+                                         "min": [0.0, 0.0, 0.0]
+                                     }
+                                 ],
+
+                                 "asset": {
+                                     "version": "2.0"
+                                 }
+                             })
+        gltf_asset.export_gltf('gltf_output/test1.gltf')
+        self.assertTrue(filecmp.cmp('gltf_output/test1.gltf', 'sample_gltf/test1.gltf', shallow=False))
